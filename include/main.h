a51d73e82e59c303f5d99a01a5bbccc6cd267b93
AWonderfulUser <example@example.org>
2023-09-10 08:58:03 +0200

    Edited a bit the configuration

--- include/main.h
+++ include/main.h
@@ -1,4 +1,4 @@
 double current = 1; // The number that we are currently dividing by
 double pi = 0; // Pi
-int precision = 1000; // The precision of the float that will be sent to the console
-float maxtries = 10E10; // The maximum attempts to find pi
+int precision = 22; // The precision of the float that will be sent to the console. max 22
+float maxtries = 10E2; // The maximum attempts to find pi
