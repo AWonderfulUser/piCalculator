96a31f9d72abd1100a8a41274dbcfb9c12e48cfc
AWonderfulUser <example@example.org>
2023-09-10 08:59:12 +0200

    Updated README

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ To build this project, you need to have CMake and make installed on your compute
 1. Switch to the "build" directory (if it isn't created create it)
 2. Execute `cmake ..`
 3. Execute `make`
+
+# Change the paramaters
+
+A basic configuration is available under include/main.h From there you can set up the precision and the maximum number of tries
--- include/main.h
+++ include/main.h
@@ -1,4 +1,6 @@
-double current = 1; // The number that we are currently dividing by
-double pi = 0; // Pi
+double current = 1; // The number that we are currently dividing by, don't touch it
+double pi = 0; // Pi, don't touch it
+
+// Acutal configuration
 int precision = 22; // The precision of the float that will be sent to the console. max 22
 float maxtries = 10E2; // The maximum attempts to find pi
