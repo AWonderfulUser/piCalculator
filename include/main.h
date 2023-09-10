176941bec88aaf268e588745a6fc84186ecd87cf
AWonderfulUser <example@example.org>
2023-09-10 08:51:44 +0200

    Made a working pi calculator

---
+++ include/main.h
@@ -0,0 +1,4 @@
+double current = 1; // The number that we are currently dividing by
+double pi = 0; // Pi
+int precision = 1000; // The precision of the float that will be sent to the console
+float maxtries = 10E10; // The maximum attempts to find pi
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
+#include <iomanip>
+#include "main.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    // Calculating pi using the Gregory-Leibniz series
+    float expr = (4/current) - (4/(current+2));
+    for(double x = 0; x < maxtries; x++) {
+        
+        current += 4;
+        expr += (4/current) - (4/(current+2));
+    }
+    std::cout << "C:" << current << " -> " << std::fixed << std::setprecision(precision) << expr << std::endl;
 }
